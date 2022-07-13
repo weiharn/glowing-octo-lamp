94b03ee03941825554f1ab454d1b34b45a96f77f
weiharn <109207352+weiharn@users.noreply.github.com>
2022-07-13 16:01:28 +0800

    Add files via upload

---
+++ pos_inventory/admin/index.php
@@ -0,0 +1,23 @@
+<?php include('session.php'); ?>
+<?php include('header.php'); ?>
+<body>
+<div id="wrapper">
+<?php include('navbar.php'); ?>
+<div style="height:50px;"></div>
+<div id="page-wrapper">
+<div class="container-fluid">
+<div style="height:40px;"></div>
+	<img src="../upload/844.png" class="thumbnail" style="height:430px; width:1030px;">
+</div>
+</div>
+</div>
+<?php include('script.php'); ?>
+<?php include('modal.php'); ?>
+</body>
+<div class="foot">
+<footer>
+<p> i HECKIN' love shirts</p>
+</footer>
+<style> .foot{text-align: center; border: 2px solid black;}</style>
+</div>
+</html>
